54cf5aad7a5c3a187cfbd6b305f6a1546af1f4c5
erlint1212 <e.t.nordtvedt@gmail.com>
2025-10-11 07:52:29 +0200

    Basic connection to RabbitMQ docker server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"log"
+    amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func err_hand(err error) {
+	log.Fatal(err)
+}
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connection_string := "amqp://guest:guest@localhost:5672/"
+
+	connection, err := amqp.Dial(connection_string)
+	if err != nil {
+		err_hand(err)
+	}
+
+	defer func() {
+		err := connection.Close()
+		if err != nil {
+			err_hand(fmt.Errorf("Error while closing server connection: %v", err))
+		}
+	}()
+
+	fmt.Println("Peril server connection succesful")
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+
+	<-sigs
+
+	fmt.Println("Peril is shutting down and close the server connection")
 }
