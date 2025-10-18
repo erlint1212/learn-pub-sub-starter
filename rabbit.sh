c7ad652998702801330cfa0d8e6609f7b90b63cc
erlint1212 <e.t.nordtvedt@gmail.com>
2025-10-18 22:52:50 +0200

    Testing with STOMP

--- rabbit.sh
+++ rabbit.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 start_or_run () {
-    docker inspect peril_rabbitmq > /dev/null 2>&1
+    docker inspect rabbitmq_stomp > /dev/null 2>&1
 
     if [ $? -eq 0 ]; then
-        echo "Starting Peril RabbitMQ container..."
-        docker start peril_rabbitmq
+        #echo "Starting Peril RabbitMQ container..."
+        echo "Starting STOMP RabbitMQ container..."
+        #docker start peril_rabbitmq
+        docker start rabbitmq_stomp
     else
         echo "Peril RabbitMQ container not found, creating a new one..."
-        docker run -d --name peril_rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        #docker run -d --name peril_rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        docker run -d --name rabbitmq_stomp -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp:latest
     fi
 }
 
@@ -18,11 +21,11 @@ case "$1" in
         ;;
     stop)
         echo "Stopping Peril RabbitMQ container..."
-        docker stop peril_rabbitmq
+        docker stop rabbitmq_stomp
         ;;
     logs)
         echo "Fetching logs for Peril RabbitMQ container..."
-        docker logs -f peril_rabbitmq
+        docker logs -f rabbitmq_stomp
         ;;
     *)
         echo "Usage: $0 {start|stop|logs}"
