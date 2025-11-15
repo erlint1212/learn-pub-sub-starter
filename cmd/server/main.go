12b66c151aafd5a5d2cf21a1851817999b7cfc98
erlint1212 <e.t.nordtvedt@gmail.com>
2025-11-15 03:49:40 +0100

    3.6 Decouple

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 	"log"
     amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 )
 
 func err_hand(err error) {
@@ -37,24 +35,46 @@ func main() {
 		err_hand(err)
 	}
 
-	err = pubsub.PublishJSON(
-		channel, 
-		routing.ExchangePerilDirect, 
-		routing.PauseKey, 
-		routing.PlayingState{IsPaused: true},
-	)
-	if err != nil {
-		err_hand(err)
-	}
-
 	fmt.Println("Peril server connection succesful")
 
+	gamelogic.PrintServerHelp()
 
-	sigs := make(chan os.Signal, 1)
+	outerLoop:
+		for {
+			input := gamelogic.GetInput()
+			if len(input) == 0 {
+				continue
+			}
+			switch input[0] {
+			case "pause":
+				log.Println("Sending pause message")
+				err = pubsub.PublishJSON(
+					channel, 
+					routing.ExchangePerilDirect, 
+					routing.PauseKey, 
+					routing.PlayingState{IsPaused: true},
+				)
+				if err != nil {
+					err_hand(err)
+				}
+			case "resume":
+				log.Println("Sending resume message")
+				err = pubsub.PublishJSON(
+					channel, 
+					routing.ExchangePerilDirect, 
+					routing.PauseKey, 
+					routing.PlayingState{IsPaused: false},
+				)
+				if err != nil {
+					err_hand(err)
+				}
+			case "quit":
+				log.Println("Shutting down")
+				break outerLoop
+			default:
+				log.Printf("Command \"%v\" not recognized", input)
 
-	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
-
-	<-sigs
-
-	fmt.Println("Peril is shutting down and close the server connection")
+			}
+		}
+	log.Println("Peril is shutting down and close the server connection")
 }
